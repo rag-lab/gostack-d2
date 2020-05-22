c1ad191c97188e143fffd3d2f4ae4e53ebc6f9c7
rag-lab <emaildorodrigo@yahoo.com>
2020-05-22 01:01:07 -0300

    FrontEnd em ReactJS consumindo API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
+import api from './services/api';
 import "./styles.css";
 
 function App() {
+
+  //useState 2 param, 1- valor inicial da var, 2 funcao para atualizar o valor.
+  const [projects, setProjects] = useState([]);
+
+
+  useEffect(()=>{
+    api.get('repositories').then( response =>{
+      setProjects(response.data)
+    })
+  }, [])
+
+
   async function handleAddRepository() {
     // TODO
+    const response = await api.post('repositories',
+      {
+        "title":`New Proj ${Date.now()}`,
+        "url":"http://banana.com",
+        "techs":["banana","nanica","da terra"]
+      }
+    )
+    const project = response.data;
+
+    setProjects([...projects, project])
   }
 
   async function handleRemoveRepository(id) {
+
     // TODO
+    await api.delete(`repositories/${id}`)
+
+    const newProjects = projects.filter(
+      project => project.id !== id
+    )
+
+    setProjects(newProjects)
+
+    //api.get('repositories').then( response =>{
+    // setProjects(response.data)      
+    //})
   }
+
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
 
-          <button onClick={() => handleRemoveRepository(1)}>
+        {projects.map(project => 
+          <li key={project.id}>{project.title}
+          <button type="button" onClick={() => handleRemoveRepository(project.id)}>
             Remover
           </button>
-        </li>
+          </li>
+          )
+        }
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
